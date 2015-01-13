b7f5fc824f7c720d8e94427f6a351bec4a58e715
cyhsieh <aabbabc12345@gmail.com>
2015-01-13 11:43:52 +0800

    add QQ table

---
+++ smallQQmain.php
@@ -0,0 +1,85 @@
+<script type="text/javascript" src="http://www.google.com/jsapi"></script> <script type="text/javascript" language="javascript"> google.load("jquery", "1.3"); </script>
+
+<?php
+date_default_timezone_set('ASIA/Taipei');
+$mytable = 'smallqq';
+
+include ('mysql.inc.php');
+
+
+
+if (!empty($_POST['item']) && !empty($_POST['amount']))
+{
+	$time = $_POST['time'];
+	$item = $_POST['item'];
+	$amount = $_POST['amount'];
+	$note = $_POST['note'];
+	
+	if (empty($time))
+	{
+		$time = date('Y-m-d');
+	}
+	
+	$stmt = mysqli_prepare($conn, "INSERT $mytable (日期時間, 項目,金額,備註) VALUES (?,?,?,?)");
+	mysqli_stmt_bind_param($stmt, 'ssis', $time,$item,$amount,$note);
+	mysqli_stmt_execute($stmt);	
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<!---->
+	<link rel = "StyleSheet" type = "text/css" href = "DBTEST.css">
+	<title>DB TEST</title>
+</head>
+
+<?php
+if (!empty($_POST["time"]))
+{
+	if ($_POST["time"] != "")
+		$nowtime = $_POST["time"];
+}
+else
+	$nowtime = date("ymd");
+?>
+
+<body onload = "inputForm.time.select()">
+	<div id = "top">
+	<form name = "inputForm" method = "post" action="<?php $_SERVER["PHP_SELF"] ?>">
+    日期：<input name = "time" value = "<?php echo "$nowtime"; ?>"><br>
+	項目：<input name = "item"><br>
+	金額：<input name = "amount"><br>
+	備註：<input name = 'note'>
+	<input name = "submit" type = "submit" value = "新增">
+	</form>
+	</div>
+	
+<?php
+
+$result = mysqli_query($conn, "SELECT 記事編號,日期時間, 項目, 金額, 備註 FROM ".$mytable);
+
+echo "
+<div id = 'mainContent'><div id = 'contentTitle' ><span class = 'heart'>♥♥</span> 大小親親來記帳 - 小親親專用 <span class = 'heart'>♥♥</span></div>
+
+
+<table><tr>
+					<th>日期時間</th><th>項目</th>
+					<th>金額</th><th>備註</th>";
+
+while ($row = mysqli_fetch_array($result)){
+// foreach($row = mysqli_fetch_array($result)){
+	echo "<tr><td>$row[1] </td>
+						<td>$row[2]</td><td>$row[3]</td><td>$row[4]</td>
+						<td><a href = 'delete.php?del={$row['記事編號']}'>刪除</a></td>
+						<td><a href = 'edit.php?edit={$row['記事編號']}'>修改</a></td></tr>";
+	// echo $row;
+	}
+echo '</table></div>';
+
+?>
+
+</body>
+</html>
